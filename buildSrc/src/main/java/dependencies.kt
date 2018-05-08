11b4957f524526565307ba5531e6df9241c4b5d9
IVIanuu <IVIanuu@gmail.com>
2018-05-08 15:41:22 +0200

    Min sdk 14

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -3,7 +3,7 @@
 object Versions {
     // android
     const val compileSdk = 27
-    const val minSdk = 17
+    const val minSdk = 14
     const val targetSdk = 27
     const val versionCode = 1
     const val versionName = "1.0"
